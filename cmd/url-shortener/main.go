e011c0b4269a1b744c5fd54876b3545793cc6d29
qeery8 <daniil.chernykh133@gmail.com>
2025-06-15 21:36:39 +0300

    add config and setup logger

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/qeery8/restApi/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting ...", slog.String("env", cfg.Env))
+	log.Debug("debug message are enabled")
+
+	// sqlite
+	// router
+	// run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
